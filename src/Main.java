8914182cd4061b7a9a685010ef4b62c4d3be9e5f
artn-dev <azemarteixeira@gmail.com>
2021-11-10 22:31:59 -0300

    music id assignment

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,36 @@
 import ui.PlayerWindow;
+import ui.AddSongWindow;
+
+import java.awt.event.ActionListener;
 
 public class Main {
+    public static int musicCount = 0;
+
     public static void main(String[] args) {
-            PlayerWindow window = new PlayerWindow(
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    "Mastn Music Player",
-                    null
-            );
+        ActionListener addSong = e -> {
+            AddSongWindow addWindow = new AddSongWindow(getSongId(), null, null);
+        };
+
+        PlayerWindow window = new PlayerWindow(
+                null,
+                null,
+                addSong,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                "Mastn Music Player",
+                null
+        );
     }
+
+    public static String getSongId() {
+        String id = Integer.toString(musicCount);
+        musicCount++;
+        return id;
+    };
 }
