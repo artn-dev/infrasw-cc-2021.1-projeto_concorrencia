f484d0f5b94d0daefbbf7b79a31b70d7d064da8a
artn-dev <azemarteixeira@gmail.com>
2021-11-10 22:09:06 -0300

    basic window

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,21 @@
+import ui.PlayerWindow;
+
 public class Main {
     public static void main(String[] args) {
+            PlayerWindow window = new PlayerWindow(
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    "Mastn Music Player",
+                    null
+            );
     }
 }
