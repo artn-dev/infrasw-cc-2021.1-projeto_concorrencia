b762fe2eb1e72aea1f7c6ea066abb79bb8b78939
artn-dev <azemarteixeira@gmail.com>
2021-11-10 22:51:15 -0300

    move backend to Player

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,5 @@
-import ui.PlayerWindow;
-import ui.AddSongWindow;
-
-import java.awt.event.ActionListener;
-
 public class Main {
-    public static int musicCount = 0;
-
     public static void main(String[] args) {
-        ActionListener addSong = e -> {
-            AddSongWindow addWindow = new AddSongWindow(getSongId(), null, null);
-        };
-
-        PlayerWindow window = new PlayerWindow(
-                null,
-                null,
-                addSong,
-                null,
-                null,
-                null,
-                null,
-                null,
-                null,
-                null,
-                null,
-                "Mastn Music Player",
-                null
-        );
+        Player foo = new Player();
     }
-
-    public static String getSongId() {
-        String id = Integer.toString(musicCount);
-        musicCount++;
-        return id;
-    };
 }
--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,37 @@
+import ui.AddSongWindow;
+import ui.PlayerWindow;
+
+import java.awt.event.ActionListener;
+
 public class Player {
+    public static int musicCount = 0;
+
     public Player() {
+        ActionListener addSong = e -> {
+            AddSongWindow addWindow = new AddSongWindow(getSongId(), null, null);
+        };
+
+        PlayerWindow window = new PlayerWindow(
+                null,
+                null,
+                addSong,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                "Mastn Music Player",
+                null
+        );
     }
+
+    public static String getSongId() {
+        String id = Integer.toString(musicCount);
+        musicCount++;
+        return id;
+    };
 }
 
