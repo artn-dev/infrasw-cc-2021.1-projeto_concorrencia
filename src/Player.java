6001e35b45d603caaea952338507105bbbc7f02c
artn-dev <azemarteixeira@gmail.com>
2021-11-10 23:20:52 -0300

    list of songs

--- src/Player.java
+++ src/Player.java
@@ -2,16 +2,27 @@ import ui.AddSongWindow;
 import ui.PlayerWindow;
 
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Player {
+    public static PlayerWindow window;
+    public static AddSongWindow addWindow;
     public static int musicCount = 0;
+    public static List<String[]> songs;
 
     public Player() {
-        ActionListener addSong = e -> {
-            AddSongWindow addWindow = new AddSongWindow(getSongId(), null, null);
+        songs = new ArrayList<String[]>();
+
+        ActionListener confirmSong = e -> {
+            songs.add(addWindow.getSong());
         };
 
-        PlayerWindow window = new PlayerWindow(
+        ActionListener addSong = e -> {
+            addWindow = new AddSongWindow(getSongId(), confirmSong, null);
+        };
+
+        window = new PlayerWindow(
                 null,
                 null,
                 addSong,
